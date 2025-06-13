4d70e785b4fea3d1e8adf50c5df1bf485b1a57ee
hau <nguyenviethau.it.2004@gmail.com>
2025-06-13 17:10:15 +0700

    use env

--- config/config.php
+++ config/config.php
@@ -1,7 +1,8 @@
 <?php
 return [
-    'GOOGLE_CLIENT_ID' => '121408504037-2v4no5i6925vc542231hatscbc7cllrd.apps.googleusercontent.com',
-    'GOOGLE_CLIENT_SECRET' => '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',
-    'GOOGLE_REDIRECT_URI' => 'https://nitroauto.cloud/auth/google/callback',
-    'OPENROUTER_API_KEY'=>'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'
+    'GOOGLE_CLIENT_ID'     => getenv('GOOGLE_CLIENT_ID'),
+    'GOOGLE_CLIENT_SECRET' => getenv('GOOGLE_CLIENT_SECRET'),
+    'GOOGLE_REDIRECT_URI'  => getenv('GOOGLE_REDIRECT_URI'),
+    'OPENROUTER_API_KEY'   => getenv('OPENROUTER_API_KEY'),
 ];
+
--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+require_once 'vendor/autoload.php';
+
+// Load biến môi trường từ file .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
     $redirect = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
@@ -8,5 +13,3 @@ if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
 
 require_once 'config/database.php';
 require_once 'routes/web.php';
-require_once 'vendor/autoload.php';
-?>
